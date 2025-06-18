7fba6e85341c9bc50dec549a8afcb2941c49d4e7
Beyzanur Senocak <beyzanursenocak96@gmail.com>
2025-06-18 11:39:15 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue']
+        }
+      }
+    }
+  },
   server: {
     port: 3000
   }
